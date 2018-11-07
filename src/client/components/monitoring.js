84e96d555a1ad44bd80d26eb69e200529088d181
Edward Roh <Admin@Eds-MacBook-Pro.local>
2018-11-07 14:24:35 -0800

    changing front end

---
+++ src/client/components/monitoring.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Monitoring = () => (
+  <div className="monitoring">
+    <h3>Monitoring</h3>
+    <div>Refresh</div>
+    <div>Toggle Live Data</div>
+  </div>
+);
+
+export default Monitoring;
